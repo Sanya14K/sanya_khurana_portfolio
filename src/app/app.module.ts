f66d828263a82eeb4a231b9dc96ec6141964b5d1
Sanya Khurana <sanya14n@gmail.com>
2025-03-31 22:39:06 +0530

    went live

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { WorkExperienceComponent } from './work-experience/work-experience.component';
+import { EducationComponent } from './education/education.component';
+import { PersonalProjectsComponent } from './personal-projects/personal-projects.component';
+import { SkillsComponent } from './skills/skills.component';
+import { ContactComponent } from './contact/contact.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    WorkExperienceComponent,
+    EducationComponent,
+    PersonalProjectsComponent,
+    SkillsComponent,
+    ContactComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/personal-projects/personal-projects.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-personal-projects',
+  standalone: false,
+  templateUrl: './personal-projects.component.html',
+  styleUrl: './personal-projects.component.scss'
+})
+export class PersonalProjectsComponent {
+
+}
